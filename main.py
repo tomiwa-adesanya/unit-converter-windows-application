c567248239ead0825ce852415772bc48306532d1
Tomiwa Adesanya <tomiwa392@gmail.com>
2022-09-25 16:48:42 +0100

    ** Updates:     * autoconverts units as user changes the value.     * conversion methods defined.     * new events binded to respective widgets.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from statistics import quantiles
 from tkinter import ttk
 from converter.converter import UnitConverterTools
 import tkinter as tk
@@ -139,12 +140,36 @@ class UnitConverter(tk.Tk):
         self._to_combobox.config(values=quantity_units)
 
         self._from_combobox_var.set(quantity_units[0])
-        self._to_combobox_var.set(quantity_units[0])
+        self._to_combobox_var.set(quantity_units[1])
+
+        self._from_entry_var.set("1")
+        self.__convert_from_to__to()
 
     def __bind_events(self):
 
         self.quantity_combobox.bind(
             "<<ComboboxSelected>>", lambda event=None: self.__quantity_selected()
+        )
+
+        self._from_entry.bind(
+            "<Return>", lambda event=None: self.__convert_from_to__to(), add="+"
+        )
+        self._from_entry_var.trace(
+            "w", lambda arg1=None, arg2=None, arg3=None: self.trace_from()
+        )
+
+        self._to_entry.bind(
+            "<Return>", lambda event=None: self.__convert_to_to__from()
+        )
+        self._to_entry_var.trace(
+            "w", lambda arg1=None, arg2=None, arg3=None: self.trace_to()
+        )
+
+        self._from_combobox.bind(
+            "<<ComboboxSelected>>", lambda event=None: self.__convert_from_to__to()
+        )
+        self._to_combobox.bind(
+            "<<ComboboxSelected>>", lambda event=None: self.__convert_from_to__to()
         )
 
         self.bind(
@@ -153,6 +178,59 @@ class UnitConverter(tk.Tk):
         self.bind(
             "<Control-W>", lambda event=None: self.destroy()
         )
+
+    def _get_quantity(self) -> str:
+        quantity = self.quantity_combobox_var.get().lower()
+        quantity = quantity.replace(" ", "_") if (" " in quantity) else quantity
+        return quantity
+    def _get__from_unit(self) -> str:
+        _from_unit = self._from_combobox_var.get().lower() 
+        _from_unit = _from_unit.replace(" ", "_") if (" " in _from_unit) else _from_unit
+        return _from_unit
+    def _get__from_value(self) -> int | float:
+        _from_value = self._from_entry_var.get()
+        _from_value = "0" if (not _from_value) else _from_value
+        _from_value = float(_from_value) if ("." in _from_value) else int(_from_value)
+        return _from_value
+    def _get__to_unit(self) -> str:
+        _to_unit = self._to_combobox_var.get().lower()
+        _to_unit = _to_unit.replace(" ", "_") if (" " in _to_unit) else _to_unit
+        return _to_unit
+    def _get__to_value(self) -> int | float:
+        _to_value = self._to_entry_var.get()
+        _to_value = "0" if (not _to_value) else _to_value
+        _to_value = float(_to_value) if ("." in _to_value) else int(_to_value)
+        return _to_value
+
+    def __convert_from_to__to(self):
+        try:
+            quantity = self._get_quantity()        
+            _from_unit = self._get__from_unit()
+            _from_value = self._get__from_value()
+            _to_unit = self._get__to_unit()
+            final = self.converter._convert(quantity, value=_from_value, _from=_from_unit, _to=_to_unit)
+            self._to_entry_var.set(str(final))
+        except:
+            pass
+
+    def __convert_to_to__from(self):
+        try:
+            quantity = self._get_quantity()
+            _to_unit = self._get__to_unit()
+            _to_value = self._get__to_value()
+            _from_unit = self._get__from_unit()
+            final = self.converter._convert(quantity=quantity, value=_to_value, _from=_to_unit, _to=_from_unit)
+            self._from_entry_var.set(str(final))
+        except:
+            pass
+    
+    def trace_from(self, arg1=None, arg2=None, arg3=None):
+        if (self.focus_get() == self._from_entry):
+            self.__convert_from_to__to()
+    
+    def trace_to(self, arg1=None, arg2=None, arg3=None):
+        if (self.focus_get() == self._to_entry):
+            self.__convert_to_to__from()
         
         
     
